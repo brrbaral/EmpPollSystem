066759702a5dfc6c1f9f2ccbb8a63c074d3a0872
brrbaral <45137751+brrbaral@users.noreply.github.com>
2019-09-28 08:42:13 +0545

    Update settings.py

--- EmpMS/settings.py
+++ EmpMS/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'brb.aarva6@gmail.com'
-EMAIL_HOST_PASSWORD = 'bishowdai'
+EMAIL_HOST_PASSWORD = '************'
 EMAIL_PORT = 587
 
 #LOGIN/LOGOUT
